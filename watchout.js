b7f955cb132b3289e8060258a553a82bdb1876c3
Hack Reactor Students <rajatkhanna82@gmail.com>
2014-04-25 16:24:01 -0700

    Created 5 enemies which move around to random locations using d3.

--- watchout.js
+++ watchout.js
@@ -1,2 +1,85 @@
 // start slingin' some d3 here.
 
+var width = 600;
+var height = 600;
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+  .append("g");
+
+
+function updateEnemies(data){
+  //datajoin
+  var enemy = svg.selectAll(".enemy")
+    .data(data, function(d){return d.id;});
+
+  //update
+  enemy.transition()
+    .duration(500)
+    .attr("cx", function(d){ return d.x;})
+    .attr("cy", function(d){ return d.y;})
+    .attr("r", 10);
+
+  //enter
+  enemy.enter().append("svg:circle")
+    .attr("class", "enemy")
+    .attr("cx", function(d){ return d.x;})
+    .attr("cy", function(d){ return d.y;})
+    .attr("r", 10);
+
+  //exit
+
+}
+
+function randomLocations(n){
+  var locArr = [];
+  for (var i=0; i<n; i++){
+    locArr.push({
+      x: Math.random()*width,
+      y: Math.random()*height,
+      id: i
+    });
+  }
+  return locArr;
+}
+
+setInterval(function(){
+  updateEnemies(randomLocations(5));
+}, 2000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
