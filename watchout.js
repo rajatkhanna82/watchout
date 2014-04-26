3bf49c4da0eaeebe256e16545deb94855650da98
Hack Reactor Students <rajatkhanna82@gmail.com>
2014-04-25 20:03:08 -0700

    Made hero draggable and added collisions between hero and enemies.

--- watchout.js
+++ watchout.js
@@ -3,18 +3,66 @@
 var width = 600;
 var height = 600;
 
+var drag = d3.behavior.drag()
+  .on("drag", function(d,i) {
+    d.x += d3.event.dx;
+    d.y += d3.event.dy;
+    d3.select(this).attr("cx", function(d){
+        return d.x;
+      })
+    .attr("cy", function(d){
+        return d.y;
+      });
+});
+
 var svg = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height)
   .append("g");
 
+var hero = svg.append("circle")
+  .data([{"x":width/2 ,"y":height/2}])
+  .attr("class", "hero")
+  .attr("fill", "orange")
+  .attr("cx", function(d){return d.x;})
+  .attr("cy", function(d){return d.y;})
+  .attr("r", 10)
+  .call(drag);
 
-function update(enemydata, herodata){
+
+var randomLocations = function(n){
+  var locArr = [];
+  for (var i=0; i<n; i++){
+    locArr.push({
+      x: Math.random()*width,
+      y: Math.random()*height,
+      id: i
+    });
+  }
+  return locArr;
+};
+
+var checkCollisions = function(){
+  var enemies = svg.selectAll(".enemy");
+  for (var i=0; i<enemies[0].length; i++){
+    var enemyX = parseInt(enemies[0][i].getAttribute("cx"));
+    var enemyY = parseInt(enemies[0][i].getAttribute("cy"));
+    var heroX = parseInt(hero.attr("cx"));
+    var heroY = parseInt(hero.attr("cy"));
+    var enemyRadius = parseInt(enemies[0][i].getAttribute("r"));
+    var heroRadius = parseInt(hero.attr("r"));
+    if ( Math.abs(heroX-enemyX) < (enemyRadius+heroRadius) &&
+      Math.abs(heroY-enemyY) < (enemyRadius+heroRadius)){
+        //score resets or something happens
+      console.log("collision detected!");
+    }
+  }
+};
+
+var update = function(enemydata){
   //datajoin
   var enemy = svg.selectAll(".enemy")
     .data(enemydata, function(d){return d.id;});
-  var hero = svg.selectAll(".hero")
-    .data(herodata, function(d){return d.id;});
 
   //update
   enemy.transition()
@@ -30,37 +78,20 @@ function update(enemydata, herodata){
     .attr("cx", function(d){ return d.x;})
     .attr("cy", function(d){ return d.y;})
     .attr("r", 10);
-  hero.enter().append("svg:circle")
-    .attr("class", "hero")
-    .attr("fill", "orange")
-    .attr("cx", function(d){ return d.x;})
-    .attr("cy", function(d){ return d.y;})
-    .attr("r", 10);
 
 
   //exit
 
-}
+};
 
 
-function randomLocations(n){
-  var locArr = [];
-  for (var i=0; i<n; i++){
-    locArr.push({
-      x: Math.random()*width,
-      y: Math.random()*height,
-      id: i
-    });
-  }
-  return locArr;
-}
+update(randomLocations(5));
 
 setInterval(function(){
-
-  update(randomLocations(5), [{x: height/2, y: width/2, id:"hero"}]);
+  update(randomLocations(5));
 }, 2000);
 
-
+setInterval(checkCollisions, 100);
 
 
 
